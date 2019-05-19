c6d69f43d218b8a0bcf0e7973a856c5239b016ff
vinod Chopra <rishabhchopra.dcs16@vnsgu.ac.in>
2019-05-19 08:41:16 +0530

    Setup Base of Project with monogoDb and basic for scanToEmail

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8101;
+const parser = require('./server/util/parser');
+const api = require('./server/routes/');
+const monogo = require('./server/MongoConfig');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+monogo.connect();
+
+// TODO:Implementing Swagger for Rest APi Test
+const swaggerDefinition = {
+    info: {
+        title: 'Brother Printer Swagger API',
+        version: '1.0.0',
+        description: 'Endpoints to test the user registration routes',
+    },
+    host: 'localhost:' + port,
+    basePath: '/',
+    securityDefinitions: {
+        bearerAuth: {
+            type: 'apiKey',
+            name: 'Authorization',
+            scheme: 'bearer',
+            in: 'header',
+        },
+    },
+};
+const options = {
+    swaggerDefinition,
+    apis: ['./server/routes/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(options);
+app.get('/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// All routes for /api are send to API Router
+app.use('/api',  api);
+app.listen(port, (err) => {
+    if(err) {
+        console.log(err)
+    }
+    console.log('server running on ', port)
+})
